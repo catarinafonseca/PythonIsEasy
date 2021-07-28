20902ab5cd312600eb8c93da78d0d02c28043472
Catarina Fonseca <catarinaconstantef@gmail.com>
2021-07-28 23:23:55 +0100

    2nd commit

--- homework3/main.py
+++ homework3/main.py
@@ -1,3 +1,7 @@
+""" Create a function that accepts 3 parameters and checks for equality between any two of them.
+
+Your function should return True if 2 or more of the parameters are equal, and false is none of them are equal to any of the others. """
+
 
 def Equality(num1, num2, num3):
     if int(num1) == int(num2) or int(num2) == int(num3) or int(num3) == int(num1):
@@ -6,5 +10,4 @@ def Equality(num1, num2, num3):
         return False
 
 
-print(Equality(6, 5,"5"))
-
+print(Equality(6, 5, "5"))
