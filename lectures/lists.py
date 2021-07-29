165df46fdcb2aea0740b289e921f80e208bc119c
Catarina Fonseca <catarinaconstantef@gmail.com>
2021-07-29 11:25:25 +0100

    new  lecture:lists

---
+++ lectures/lists.py
@@ -0,0 +1,18 @@
+# basic list
+# TestList=["element1", "element2","element3"]
+# TestList = [0, 1, 2]
+
+scores = [70, 85, 67.5, 90, 80]
+print(scores)
+print(scores[1])
+# access 0 up to 2 but not including 2
+print(scores[0:2])
+
+#scores[2]=["Hello", "World"]
+print(scores)
+# to access the 1st element in position 2
+# print(scores[2][0])
+
+# to add an element
+scores.append(82)
+print(scores)
